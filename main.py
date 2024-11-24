cb001d85a6fa5bf88d05fc32803c1758c39f3959
MakCimka <poduskapuska@gmail.com>
2024-11-24 17:54:45 +0300

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import random
 numbers = [-3, -5, -2, -12, 0, 15, 4, 7, 2]
 sum = 0
-y = list()
-weight =random.randint(4, 4)
-length = random.randint(4, 4)
-for i in range(weight) :
-    y = [random.choice(numbers) for i in range(length)]
-    print(y)
-for i in numbers: 
-    if i < 0:
-        sum += i
+weight = random.randint (4, 8)
+length = random.randint(4, 8)
+y = [[random.choice(numbers) for i in range(weight)] for j in range(length)]
+for i in range (length):
+    for j in range(weight):
+        print(y[i][j], end = ' ')
+    print()
+for row in y:
+    for element in row: 
+            if element < 0:
+                sum += element
 print(f"Сумма всех отрицательных элементов равнa : {sum}")
-
